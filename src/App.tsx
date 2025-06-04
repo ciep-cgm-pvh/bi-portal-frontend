c5ad26537790f1bb0d063d7a83025368d2e00127
Jhonatan Silva Carvalho <jhonatan.silva.4198@gmail.com>
2025-06-04 13:21:48 -0400

    project start!
    Vite + React + TS + TailwindCSS;
    
    Custom Colors at index.css;

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+function App() {
+  return (
+    <>
+      <div className="bg-white py-24 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <h1 className="text-center text-9xl font-semibold text-midnight">
+          Hello World!
+        </h1>
+        <p className="mt-6 text-center text-4xl leading-8 text-gray-600">
+          This is a simple  <span className='text-cyan-500'>React app</span> styled with <span className='text-cyan-500'>Tailwind CSS</span>.
+        </p>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
