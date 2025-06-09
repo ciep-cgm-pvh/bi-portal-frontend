f37e56aa78e2f1579cd3c3d121ffd1b053f7ab15
Clara Helena Alencar Sena <clarahelenasena@gmail.com>
2025-06-09 11:17:42 -0400

    feat: add LayoutBase.tsx
    add layout component, useIsMobile hook, and minor fix on app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-d
 import NotFound from './pages/NotFound';
 import Home from './pages/Home';
 import getNavLinks from './data/NavLinksData';
+import Layout from './components/LayoutBase';
 
 function App() {
   const navLinks = getNavLinks();
@@ -11,10 +12,13 @@ function App() {
         <Route path="/" element={<Navigate to="/home" />} />
         <Route path="/home" element={<Home />} />
 
-        {/* Rotas geradas dinamicamente */}
-        {navLinks.map(({ path, element }) => (
-          <Route key={path} path={path} element={element} />
-        ))}
+        {/* Rota estática com o layout da Navbar e Sidebar */}
+        <Route element={<Layout />}>
+          {/* Rotas dinamicas */}
+          {navLinks.map(({ path, element }) => (
+            <Route key={path} path={path} element={element} />
+          ))}
+        </Route>
         {/* Leva o usuário a página not found caso a rota não exista */}
         <Route path="/*" element={<NotFound />} />
       </Routes>
