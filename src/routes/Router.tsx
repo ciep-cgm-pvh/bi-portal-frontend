a826caebb8afc7374b0d8ff9065f169fb789fe91
Jhonatan Silva Carvalho <jhonatan.silva.4198@gmail.com>
2025-06-18 10:35:03 -0400

    major updates

--- src/routes/Router.tsx
+++ src/routes/Router.tsx
@@ -3,6 +3,7 @@ import { Navigate, Route, Routes } from "react-router-dom";
 import getNavLinks from "../data/NavLinksData";
 import LayoutBase from '../layouts/LayoutBase/LayoutBase';
 import Home from "../pages/Home/Home";
+import HubPage from '../pages/Hub/Hub';
 import NotFound from "../pages/NotFound/NotFound";
 
 function Router() {
@@ -10,18 +11,23 @@ function Router() {
 
   return (
       <Routes>
-        <Route path="/" element={<Navigate to="/home" />} />
-        
+      {/* Redirecionamento raiz */}
+      <Route path="/" element={<Navigate to="/home" />} />
 
-        <Route element={<LayoutBase />}>
-          <Route path="/home" element={<Home />} />
-          {navLinks.map(({ path, element }) => (
-            <Route key={path} path={path} element={element} />
-          ))}
-        </Route>
+      {/* Página do hub - FORA DO LAYOUT */}
+      <Route path="/hub" element={<HubPage />} />
 
-        <Route path="/*" element={<NotFound />} />
-      </Routes>
+      {/* Demais rotas COM layout base (com sidebar/navbar) */}
+      <Route element={<LayoutBase />}>
+        <Route path="/home" element={<Home />} />
+        {navLinks.map(({ path, element }) => (
+          <Route key={path} path={path} element={element} />
+        ))}
+      </Route>
+
+      {/* Página 404 */}
+      <Route path="/*" element={<NotFound />} />
+    </Routes>
   );
 }
 
