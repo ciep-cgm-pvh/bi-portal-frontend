9d205525e970f6bca8eb2d5d7b74b1ad3cc7c556
Jhonatan Silva Carvalho <jhonatan.silva.4198@gmail.com>
2025-08-06 12:04:37 -0400

    new panel - abastecimento - KPI

--- src/pages/panels/Combustivel/Dashboard/Dashboard.tsx
+++ src/pages/panels/Combustivel/Dashboard/Dashboard.tsx
@@ -1,15 +1,64 @@
+import { FuelIcon } from 'lucide-react';
+import type { JSX, Key } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Header from '../../../../components/Header/Header';
+import { KPICard, KPISection } from '../../../../components/KPI/KPISection';
+
+
+const buttonFunction = () => {
+  const navigate = useNavigate();
+  navigate('/hub');
+};
+const lastUpdate = 'dd/mm/yyyy';
+const KPIData = [
+  {
+    title: 'Gastos Totais',
+    value: 'R$ 10.000,00',
+    icon: <FuelIcon />,
+  },
+  {
+    title: 'Média Diária',
+    value: 'R$ 500,00',
+    icon: <FuelIcon />,
+  },
+  {
+    title: 'Abastecimentos',
+    value: '20',
+    icon: <FuelIcon />,
+  },
+];
+
+const AbastecimentoConfig = {
+  title: 'Abastecimento',
+  description: 'Visualize e filtre os dados de gastos com combustível.',
+  buttonFunction: buttonFunction,
+  lastUpdate: lastUpdate,
+  icon: <FuelIcon />,
+  kpiData: KPIData,
+};
+const KPIAbastecimento = ({ kpiDataList }: { kpiDataList: { icon: JSX.Element; title: string; value: string; }[] }) => {
+  return (
+    <KPISection>
+      {kpiDataList.map((kpi: { icon: JSX.Element; title: string; value: string; }, index: Key | null | undefined) => (
+        <KPICard
+          key={index}
+          icon={kpi.icon}
+          title={kpi.title}
+          value={kpi.value}
+        />
+      ))}
+    </KPISection>
+  );
+}
 
 const DashboardAbastecimento = () => {
-  const navigate = useNavigate();
 
   return (
     <div>
-      <Header title={'Abastecimento'} description={'Visualize e filtre os dados de gastos com combustível.'} onBackToHub={() => navigate('/hub')} lastUpdate={'dd/mm/yyyy'}/>
-      
+      <Header title={AbastecimentoConfig.title} description={AbastecimentoConfig.description} onBackToHub={AbastecimentoConfig.buttonFunction} lastUpdate={AbastecimentoConfig.lastUpdate}/>
+      <KPIAbastecimento kpiDataList={AbastecimentoConfig.kpiData} />
     </div>
-    
+
   );
 }
 
