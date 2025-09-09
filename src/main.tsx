f6670f8c8ed26850f7cf36dac0f2ebffb2c2821d
Jhonatan Silva Carvalho <jhonatan.silva.4198@gmail.com>
2025-09-09 11:41:41 -0400

    GRAPHQL VERCEL API

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,15 @@ import { cacheExchange, createClient, fetchExchange, Provider } from 'urql';
 import App from './App.tsx';
 import './styles/index.css';
 
-const urls ={ local: 'http://localhost:3000/graphql', localNetwork: 'http://10.148.2.210:3000/graphql'}
+const urls ={
+  local: 'http://localhost:3000/graphql',
+  localNetwork: 'http://10.148.2.210:3000/graphql',
+  production: 'https://bi-portal-api.vercel.app/graphql' 
+}
+
 // Crie o cliente, apontando para a URL do seu backend GraphQL
 const client = createClient({
-  url: urls.localNetwork,
+  url: process.env.NODE_ENV === 'PRODUCTION' ? urls.production : urls.localNetwork,
   exchanges: [
     cacheExchange, // Primeiro, tenta responder do cache
     fetchExchange, // Depois, envia a requisição pela rede
